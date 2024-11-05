6d6aead9ead87e6a81c3c3c45cbad71c6af6adc7
Tăng Tường Thoại <thoaitang24603@gmail.com>
2024-11-05 18:17:41 +0700

    add config

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/* tailwind.config.cjs */
+/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust based on your source file extensions
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
